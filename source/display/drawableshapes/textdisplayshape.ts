1fef20a42f5c48e8d7e630fd26397be8c6ceaea8
Mark McKellar <mm@mygn.myriad.com>
2018-05-21 15:28:15 -0600

    text box stuff

--- source/display/drawableshapes/textdisplayshape.ts
+++ source/display/drawableshapes/textdisplayshape.ts
@@ -120,7 +120,7 @@ export class TextDisplayShape implements Drawable
 	    if(radiusAverage!=0) radiusAverage = (radiusAverage / node.nodes.length);
 	    radiusAverage += this.displayInfo.borderWidth*5;
 	    
-	    var junctionText = node.name;	    
+	    var junctionTexnewt = node.name;	    
 	    var rectPadding = this.displayInfo.fontPixelHeight/2;
 	    
 	    canvasHolder.context.font=this.displayInfo.fontStyle+" "+this.displayInfo.fontPixelHeight+"px "+this.displayInfo.fontFace; 
@@ -233,6 +233,63 @@ export class TextDisplayShape implements Drawable
 	    	subNode.graphData.nodeDisplay.drawNode(node.canvasHolder,subNode);
 	    }	
 	}
+
+	fillTextMutipleLines(context,text,x,y,lineHeight,splitChar)
+	{
+		var lines = text.split(splitChar);
+	    var line = '';
+	
+	    for(var n = 0; n < lines.length; n++)
+	    {
+	      var metrics = context.measureText(lines[n]);
+	      context.fillText(lines[n], x, y);
+	      y = y+lineHeight; 
+	    }
+	    context.fillText(line, x, y);
+	 }
+	
+	metricsTextMutipleLines(context,text,lineHeight,splitChar)
+	{
+		var lines = text.split(splitChar);
+	    var line = '';
+	    var maxWidth = 0;
+	    var totalHeight = 0;
+	    for(var n = 0; n < lines.length; n++)
+	    {
+	      var metrics = context.measureText(lines[n]);
+	      if(metrics.width>maxWidth) maxWidth = metrics.width;
+	      totalHeight = totalHeight + lineHeight;
+	    }
+	    return({width:maxWidth,height:totalHeight});
+	 }
+	
+	roundedRect(context,x,y,w,h,r,borderWitdh,borderColor,rectColor)
+	{
+		  if (w < 2 * r) r = w / 2;
+		  if (h < 2 * r) r = h / 2;
+		  context.beginPath();
+		  context.moveTo(x+r, y);
+		  context.arcTo(x+w, y,   x+w, y+h, r);
+		  context.arcTo(x+w, y+h, x,   y+h, r);
+		  context.arcTo(x,   y+h, x,   y,   r);
+		  context.arcTo(x,   y,   x+w, y,   r);
+		  context.closePath();
+		
+	    //context.beginPath();
+	    //context.moveTo(x, y);
+	    //context.lineTo(x + width - cornerRadius, y);
+	    //context.arcTo(x + width, y, x + width, y + cornerRadius, cornerRadius);
+	    //context.lineTo(x + width, y + height);
+	    
+	  context.lineWidth = borderWitdh;
+	  context.fillStyle = rectColor;
+	  context.strokeStyle = borderColor;
+	  
+	  context.stroke();
+	  context.fill();
+  
+  }
+
 ******************************/
 
 
